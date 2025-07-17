f037e2d0ab6533d171349cffb648580168f985fe
Dhvani Shah <dhvanihshah06@gmail.com>
2025-07-17 11:17:38 +0530

    Lab

--- L18B2.js
+++ L18B2.js
@@ -1,87 +1,67 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
-const app = http.createServer((req,res)=>{
-    if(req.url == '/home.html')
-    {
-        fs.readFile('./home.html',(err,data)=>{
-            if(err)
-            {
-                res.statusCode = 404;
-                res.end("Page Not Found");
-            }
-            else{
-                res.statusCode = 200;
-                res.setHeader('Content-Type','text/html');
-                res.end(data);
-            }
-        })
-    }
-    else if(req.url == '/about.html')
-    {
-        fs.readFile('./about.html',(err,data)=>{
-            if(err)
-            {
-                res.statusCode = 404;
-                res.end("Page Not Found");
-            }
-            else{
-                res.statusCode = 200;
-                res.setHeader('Content-Type','text/html');
-                res.end(data);
-            }
-        })
-    }
-    else if(req.url == '/contact.html'){
-         fs.readFile('./contact.html',(err,data)=>{
-            if(err)
-            {
-                res.statusCode = 404;
-                res.end("Page Not Found");
-            }
-            else{
-                res.statusCode = 200;
-                res.setHeader('Content-Type','text/html');
-                res.end(data);
-            }
-        })
-    }
-    else if(req.url =='/profile.html')
-    {
-         fs.readFile('./profile.html',(err,data)=>{
-            if(err)
-            {
-                res.statusCode = 404;
-                res.end("Page Not Found");
-            }
-            else{
-                res.statusCode = 200;
-                res.setHeader('Content-Type','text/html');
-                res.end(data);
-            }
-        })
-    }
-    else if(req.url == '/ProductCrud.html')
-    {
-         fs.readFile('./ProductCrud.html',(err,data)=>{
-            if(err)
-            {
-                res.statusCode = 404;
-                res.end("Page Not Found");
-            }
-            else{
-                res.statusCode = 200;
-                res.setHeader('Content-Type','text/html');
-                res.end(data);
-            }
-        })
-    }
-    else{
-        res.end('Page Not Found');
-    }
+const app = http.createServer((req, res) => {
+  if (req.url == "/home.html") {
+    fs.readFile("./home.html", (err, data) => {
+      if (err) {
+        res.statusCode = 404;
+        res.end("Page Not Found");
+      } else {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.end(data);
+      }
+    });
+  } else if (req.url == "/about.html") {
+    fs.readFile("./about.html", (err, data) => {
+      if (err) {
+        res.statusCode = 404;
+        res.end("Page Not Found");
+      } else {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.end(data);
+      }
+    });
+  } else if (req.url == "/contact.html") {
+    fs.readFile("./contact.html", (err, data) => {
+      if (err) {
+        res.statusCode = 404;
+        res.end("Page Not Found");
+      } else {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.end(data);
+      }
+    });
+  } else if (req.url == "/profile.html") {
+    fs.readFile("./profile.html", (err, data) => {
+      if (err) {
+        res.statusCode = 404;
+        res.end("Page Not Found");
+      } else {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.end(data);
+      }
+    });
+  } else if (req.url == "/ProductCrud.html") {
+    fs.readFile("./ProductCrud.html", (err, data) => {
+      if (err) {
+        res.statusCode = 404;
+        res.end("Page Not Found");
+      } else {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.end(data);
+      }
+    });
+  } else {
+    res.end("Page Not Found");
+  }
+});
 
-    
-})
-app.listen(3400,()=>{
-    console.log("Server Properly Runned");
-})
+app.listen(3400, () => {
+  console.log("Server Properly Runned");
+});
